1fb47e89b6cdd8a79c2bceca4c20420604aba023
Vincent <840014803@qq.com>
2026-03-08 21:09:57 +0000

    remove

--- src/lobd/main.cpp
+++ src/lobd/main.cpp
@@ -60,7 +60,7 @@ int main(int argc, char** argv) {
     }
 
     // Mark end of response for this seq
-    // Because matching may emit multiple events per imcomin
+    // Because matching may emit multiple events per incoming NewOrde
     {
       ts::wire::Frame endf;
       ts::wire::encode(ts::proto::ResponseEnd{}, seq, endf);
