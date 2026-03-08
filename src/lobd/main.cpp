86ad0c654f9bd4dc5da48b0a8f8b3ab905a3ac0d
Vincent <840014803@qq.com>
2026-03-08 21:07:13 +0000

    add lobd main

--- src/lobd/main.cpp
+++ src/lobd/main.cpp
@@ -60,7 +60,7 @@ int main(int argc, char** argv) {
     }
 
     // Mark end of response for this seq
-    // Because matching may emit multiple events per imc
+    // Because matching may emit multiple events per imcomin
     {
       ts::wire::Frame endf;
       ts::wire::encode(ts::proto::ResponseEnd{}, seq, endf);
