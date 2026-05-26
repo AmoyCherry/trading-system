b43a37a028567f4269fd94d1116482d39b35b79f
AmoyCherry <840014803@qq.com>
2026-05-26 17:10:07 -0400

    add get_file_name

--- src/common/time/clock.cpp
+++ src/common/time/clock.cpp
@@ -1,5 +1,7 @@
 #include "time/clock.hpp"
 #include <ctime>
+#include <chrono>
+#include <format>
 
 namespace ts::time {
 
@@ -10,4 +12,9 @@ std::uint64_t now_ns() noexcept {
          + static_cast<std::uint64_t>(ts.tv_nsec);
 }
 
+std::string get_filename_with_ymds(const std::string& base) {
+    auto now = std::chrono::system_clock::now();
+    std::string suffix = std::format("{:%Y%m%d_%H%M%S}", now);
+    return std::format("{}_{}.csv", base, suffix);
+}
 } // namespace ts::time
--- src/common/time/clock.hpp
+++ src/common/time/clock.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include <cstdint>
+#include <string>
 
 namespace ts::time {
 std::uint64_t now_ns() noexcept;
+std::string get_filename_with_ymds(const std::string& base);
 } // namespace ts::time
